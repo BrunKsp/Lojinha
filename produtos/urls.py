ada7cc4e42d2227f5e51e6c3247b1b38b4f77bfb
BrunoSiqueira <bbrunosiqueira14@gmail.com>
2022-11-28 18:04:16 -0300

    Adicionado Login e Logout

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,14 +1,14 @@
 
 from django.urls import path 
-from . import views
+from produtos.views import ProdutoView
 
 
 urlpatterns = [
-    path('',views.listaProd,name = "produto-list"),
-    path('produto/<int:id>',views.produtoView,name = "produto-view"),
-    path('newproduto/', views.newProduto, name="new-produto"),
-    path('edit/<int:id>',views.editProduto,name = "edit-produto"),
-    path('delete/<int:id>',views.deleteProduto,name = "delete-produto"),
-    path('sobrenos/',views.sobreNos ,name= "sobre-nos"),
+    path('',ProdutoView.listaProd,name = "produto-list"),
+    path('produto/<int:id>',ProdutoView.produtoView,name = "produto-view"),
+    path('newproduto/', ProdutoView.newProduto, name="new-produto"),
+    path('edit/<int:id>',ProdutoView.editProduto,name = "edit-produto"),
+    path('delete/<int:id>',ProdutoView.deleteProduto,name = "delete-produto"),
+    path('sobrenos/',ProdutoView.sobreNos ,name= "sobre-nos"),
     
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -3,71 +3,71 @@ from django.contrib.auth.decorators import login_required
 from .models import Produto
 from .forms import ProdutoForm
 from django.contrib import messages
-from django.core.paginator import Paginator
 
 
-def listaProd (request):
-    
-    search = request.GET.get('search')
+class ProdutoView():
+    def listaProd (request):
+        
+        search = request.GET.get('search')
 
-    if search :
-        produtos = Produto.objects.filter(name_icontains=search)
-    else :
-        produtos = Produto.objects.all()
-        return render(request,'produto/ListaProd.html',{'produto':produtos})
-
-
-def produtoView (request ,id):
-    produto = get_object_or_404(Produto ,pk=id)
-    return render(request,'produto/produto.html',{'produto':produto})
-
-
-
-@login_required
-def newProduto (request):
-
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST)
-
-        if form.is_valid():
-            produtos = form.save(commit=False)
-            produtos.save()
-            return redirect('/')
-
-
-    else:
-        form = ProdutoForm()
-        return render(request,'produto/addproduto.html',{'form':form})
-
-@login_required
-def editProduto (request,id):
-    produtos = get_object_or_404(Produto ,pk = id)
-    form = ProdutoForm(instance= produtos)
-
-
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST,instance=produtos)
-        if (form.is_valid()):
-            produtos.save()
-            return redirect('/')
+        if search :
+            produtos = Produto.objects.filter(name_icontains=search)
         else :
+            produtos = Produto.objects.all()
+            return render(request,'produto/ListaProd.html',{'produto':produtos})
+
+
+    def produtoView (request ,id):
+        produto = get_object_or_404(Produto ,pk=id)
+        return render(request,'produto/produto.html',{'produto':produto})
+
+
+
+    @login_required
+    def newProduto (request):
+
+        if request.method == 'POST':
+            form = ProdutoForm(request.POST)
+
+            if form.is_valid():
+                produtos = form.save(commit=False)
+                produtos.save()
+                return redirect('/')
+
+
+        else:
+            form = ProdutoForm()
+            return render(request,'produto/addproduto.html',{'form':form})
+
+    @login_required
+    def editProduto (request,id):
+        produtos = get_object_or_404(Produto ,pk = id)
+        form = ProdutoForm(instance= produtos)
+
+
+        if request.method == 'POST':
+            form = ProdutoForm(request.POST,instance=produtos)
+            if (form.is_valid()):
+                produtos.save()
+                return redirect('/')
+            else :
+                return render(request,'produto/editproduto.html',{'form':form, 'produto':produtos})
+
+
+        else:
             return render(request,'produto/editproduto.html',{'form':form, 'produto':produtos})
 
+    @login_required
+    def deleteProduto (request ,id):
+        produtos = get_object_or_404(Produto ,pk = id)
+        produtos.delete()
 
-    else:
-        return render(request,'produto/editproduto.html',{'form':form, 'produto':produtos})
-
-@login_required
-def deleteProduto (request ,id):
-    produtos = get_object_or_404(Produto ,pk = id)
-    produtos.delete()
-
-    messages.info(request,'Tarefa Deletada com Sucesso')
-    return redirect('/')
+        messages.info(request,'Tarefa Deletada com Sucesso')
+        return redirect('/')
 
 
-def sobreNos (request) :
+    def sobreNos (request) :
 
-    if request.method == 'GET':
-        return render(request,'sobrenos.html')
- 
+        if request.method == 'GET':
+            return render(request,'sobrenos.html')
+    
